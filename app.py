56f70d4c1ffb552c345844cd8c67fd2f0e9fd88d
Zeelshah12 <38009063+Zeelshah12@users.noreply.github.com>
2020-06-07 15:48:58 -0300

    Update app.py
    Update graph

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
 # -*- coding: utf-8 -*-
 """
+Created on Sun Jun  7 10:00:00 2020
+
+@author: Dell
+"""
+
+# -*- coding: utf-8 -*-
+"""
 Created on Thu Jun  4 09:58:55 2020
 
-@author: Zeel
+@author: Dell
 """
 
 from flask import Flask, render_template, request
 import requests
 import json
 import plotly
+import time
 import plotly.graph_objs as go
 
 app = Flask(__name__)
@@ -18,43 +26,49 @@ output=""
 url=""
 result=""
 
-
+#HOME PAGE 
 @app.route('/', methods = ['GET'])
 def index():
     global result
     result=""
     result= result + 'Processing default request\n'
     return render_template('zoomindex.html', result=result)
-    
-
+   
+#start function will be called while clicking on start button, multiple traces will be plotted    
 class START:
-    @app.route('/start', methods = ['POST','GET'])
-    def start():
-            global result
-            result= result + 'Processing start request\n'
-            response = requests.get("http://flaskosa.herokuapp.com/cmd/TRACE")
-            r= response.content
-            data= json.loads(r)
-    
-            labels = []
-            values = []    
-            for i in range(len(data['xdata'])):
-                labels.append((0.000299792458)/data['xdata'][i])
-            for j in range(len(data['ydata'])):
-                values.append(data['ydata'][j])
+  @app.route('/start', methods = ['POST','GET'])
+   def start():
+                global result
+                result= result + 'Processing start request\n'
+                response_start = requests.get("http://flaskosa.herokuapp.com/cmd/START")
+                rs= response_start.content
+                
+                response = requests.get("http://flaskosa.herokuapp.com/cmd/TRACE")
+                r= response.content
+                data= json.loads(r)
         
-            xScale = labels
-            yScale = values
-     
-        # Create a trace
-            trace = go.Scatter(
-            x = xScale,
-            y = yScale)
-     
-            data = [trace]
-            graphJSON = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)
-            return render_template('zoomindex.html', graphJSON=graphJSON, result=result)
+                labels = []
+                values = []    
+                for i in range(len(data['xdata'])):
+                    labels.append((0.000299792458)/data['xdata'][i])
+                for j in range(len(data['ydata'])):
+                    values.append(data['ydata'][j])
+            
+                xScale = labels
+                yScale = values
+         
+            # Create a trace
+                trace = go.Scatter(
+                x = xScale,
+                y = yScale)
+         
+                data = [trace]
+                graphJSON = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)
+                print(graphJSON)
+                return render_template('zoomindex.html', stop_output=rs, graphJSON=graphJSON, result=result)
+
         
+#single function will be called while clicking on single button 
 class SINGLE:
     @app.route('/single', methods = ['POST','GET'])
     def single():
@@ -80,9 +94,10 @@ class SINGLE:
             y = yScale)
      
             data = [trace]
-            graphJSON = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)
-            return render_template('zoomindex.html', graphJSON=graphJSON, result=result)        
-    
+            graphJSON_single = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)
+            return render_template('zoomindex.html', graphJSON_single=graphJSON_single, result=result)        
+
+#stop function will be called while clicking on stop button, and acquisition will be stopped         
 class stop:
     @app.route('/stop', methods = ['POST'])
     def stop():
@@ -93,13 +108,14 @@ class stop:
         stop_output="Machine state has stopped"
         return render_template("zoomindex.html",stop_output=stop_output, result=result)    
     
-
+#Route for echo string
 @app.route('/echo/<output>')
 def get_query(output):   
     global result
     result= result + 'Processing echo request\n'
     return render_template("zoomindex.html",output=output,result=result) 
 
+#Route for PING command
 @app.route('/PING')
 def ping():   
     global result
@@ -107,6 +123,7 @@ def ping():
     output="PONG"
     return render_template("zoomindex.html",output=output, result=result) 
 
+#Route for IDN command
 @app.route('/IDN')
 def idn():   
     global result
@@ -115,6 +132,7 @@ def idn():
     response= response.content
     return render_template("zoomindex.html",output=response, result=result) 
 
+#Route for Limits
 @app.route('/LIM')
 def lim():   
     global result
@@ -123,6 +141,7 @@ def lim():
     response= response.content
     return render_template("zoomindex.html",output=response, result=result) 
 
+#Route for knowing the machine state
 @app.route('/state')
 def state():   
     global result
@@ -131,6 +150,7 @@ def state():
     response= response.content
     return render_template("zoomindex.html",output=response, result=result) 
 
+#Route for trace
 @app.route('/trace')
 def trace():   
     global result
@@ -139,6 +159,7 @@ def trace():
     response= response.content
     return render_template("zoomindex.html",output=response, result=result) 
  
+#Route for different queries for user    
 @app.route('/query', methods = ['POST'])
 def query():
     # response_start = requests.get("http://flaskosa.herokuapp.com/cmd/STOP")
@@ -159,6 +180,12 @@ def query():
         result= result + 'Returning machine state\n'
         return render_template("zoomindex.html",output=output, result=result)
     
+    elif(q_response.lower()=="lim"):
+        response = requests.get("http://flaskosa.herokuapp.com/cmd/LIM")
+        response= response.content
+        result= result + 'Returning limits\n'
+        return render_template("zoomindex.html",output=response, result=result)
+    
     else:
     
         result= result + 'Error: Please check your query\n'
@@ -166,11 +193,6 @@ def query():
         return render_template("zoomindex.html",output=output, result=result)
         
     
-    
-    
-    
-     
-             
     
 if __name__ == '__main__':
     app.run()
